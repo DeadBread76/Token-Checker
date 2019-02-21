4b3b20b2b4a69f34bab94815cc162c907efc7973
blakeando <blakeando@live.com>
2019-02-21 19:16:24 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 from tkinter import *
 from tkinter.filedialog import *
+from colorama import init
+from termcolor import colored
 
+init()
 vcounter = 0
 icounter = 0
 print("Select the text file containing the tokens to check...")
@@ -10,11 +13,11 @@ token = askopenfilename()
 with open(token) as handle:
     txtf = handle.read()
 
-print ("Checking tokens...")
-
 f = open("tokens.txt","a")
-f.write(txtf)
+f.write("\n" + txtf)
 f.close()
+
+print ("Checking tokens...")
 
 with open('tokens.txt','r') as handle:
         tokens = handle.readlines()
@@ -26,19 +29,19 @@ with open('tokens.txt','r') as handle:
             src = requests.get('https://discordapp.com/api/v6/auth/login', headers=headers)
             try:
                 if src.status_code == 200:
-                    print(token + ' Valid.')
+                    print (colored(token + ' Valid.',"green"))
                     vcounter +=1
                     with open('valid.txt','a') as handle:
                         handle.write(token + '\n')
                 else:
-                    print(token + ' Invalid.')
+                    print(colored(token + ' Invalid.',"red"))
                     icounter +=1
                     with open('invalid.txt','a') as handle:
                         handle.write(token + '\n')
             except Exception:
                 print("Yeah we can't contact discordapp.com")
         print ("---------------------------------------")
-        print ("Number of valid tokens: " + str(vcounter))
-        print ("Number of invalid tokens: " + str(icounter))
+        print (colored("Number of valid tokens: " + str(vcounter),"green"))
+        print (colored("Number of invalid tokens: " + str(icounter),"red"))
         print ("---------------------------------------")
         input()
